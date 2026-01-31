b25a3a0fde4fc5774fa4c173389dddb7489a316d
Dale Harrison <daleharrison@MacBook-Pro-3.local>
2026-01-31 20:22:08 +0000

    Add missing ECS task role for SQS

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -133,6 +133,29 @@ resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+# 7. Task Role (for application SQS access)
+resource "aws_iam_role" "ecs_task_role" {
+  name = "ecs_task_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_sqs_policy" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
+}
+
 # 6. ECS Service (Fargate)
 resource "aws_ecs_service" "app_service" {
   name            = "spring-boot-hello-service"
