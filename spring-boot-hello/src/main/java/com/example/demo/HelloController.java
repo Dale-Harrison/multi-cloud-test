bf8cb47a29546767fab1b54360a88334933b79ac
Dale Harrison <daleharrison@MacBook-Pro-3.local>
2026-02-01 18:33:22 +0000

    Add logging to hello service for easier debugging

--- spring-boot-hello/src/main/java/com/example/demo/HelloController.java
+++ spring-boot-hello/src/main/java/com/example/demo/HelloController.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
     private final MessagePublisher messagePublisher;
 
     public HelloController(MessagePublisher messagePublisher) {
@@ -14,7 +17,10 @@ public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        messagePublisher.publish("Hello from " + System.getenv("AWS_EXECUTION_ENV"));
+        String msg = "Hello from "
+                + (System.getenv("AWS_EXECUTION_ENV") != null ? System.getenv("AWS_EXECUTION_ENV") : "GCP");
+        logger.info("Triggered home endpoint. Publishing message: {}", msg);
+        messagePublisher.publish(msg);
         return "Hello World from Cloud Run!";
     }
 }
--- spring-boot-hello/src/main/java/com/example/demo/PubSubMessagePublisher.java
+++ spring-boot-hello/src/main/java/com/example/demo/PubSubMessagePublisher.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import com.google.cloud.spring.pubsub.core.PubSubTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Service;
 @Profile("gcp")
 public class PubSubMessagePublisher implements MessagePublisher {
 
+    private static final Logger logger = LoggerFactory.getLogger(PubSubMessagePublisher.class);
     private final PubSubTemplate pubSubTemplate;
 
     public PubSubMessagePublisher(PubSubTemplate pubSubTemplate) {
@@ -16,6 +19,7 @@ public class PubSubMessagePublisher implements MessagePublisher {
 
     @Override
     public void publish(String message) {
+        logger.info("Publishing to Pub/Sub topic hello-topic: {}", message);
         pubSubTemplate.publish("hello-topic", message);
     }
 }
