2f5a60860d201b40056d34a9ea341ee998ea6971
Dale Harrison <daleharrison@MacBook-Pro-3.local>
2026-02-01 17:14:58 +0000

    Start PubSub adapter asynchronously to prevent blocking

--- spring-boot-worker/src/main/java/com/example/demo/PubSubConfig.java
+++ spring-boot-worker/src/main/java/com/example/demo/PubSubConfig.java
@@ -8,6 +8,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.boot.CommandLineRunner;
+import com.google.cloud.spring.pubsub.support.BasicAcknowledgeablePubsubMessage;
+import com.google.cloud.spring.pubsub.support.GcpPubSubHeaders;
+import org.springframework.integration.acks.AckUtils;
+import org.springframework.integration.acks.AcknowledgmentCallback;
 
 @Configuration
 @Profile("gcp")
@@ -24,6 +29,19 @@ public class PubSubConfig {
             PubSubTemplate pubSubTemplate) {
         PubSubInboundChannelAdapter adapter = new PubSubInboundChannelAdapter(pubSubTemplate, "hello-sub");
         adapter.setOutputChannel(inputChannel);
+        adapter.setAckMode(com.google.cloud.spring.pubsub.integration.AckMode.MANUAL);
+        adapter.setAutoStartup(false); // Disable auto-startup to prevent blocking
         return adapter;
+    }
+
+    @Bean
+    public CommandLineRunner startupRunner(PubSubInboundChannelAdapter adapter) {
+        return args -> {
+            new Thread(() -> {
+                System.out.println("Starting PubSubAdapter in background thread...");
+                adapter.start();
+                System.out.println("PubSubAdapter started.");
+            }).start();
+        };
     }
 }
